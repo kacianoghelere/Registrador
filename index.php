69bf073b652d52e4ed4010e17c1afc61762d7900
win <win@192.168.254.81>
2013-07-02 09:11:52 -0300

    Background adicionado e alguns bugs visuais corrigidos.

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
-    <?php include('header.php'); ?>
+        <?php include('header.php'); ?>
+        <style type="text/css">
+            body { 
+                background-image: url("image/bg.png");
+            }
+        </style>
     </head>
     <body>
         <script type="text/javascript" src="js/validalogin.js"></script>
-        <div class="well-small offset5"  align="center" style="width: 500px; background-color:#CDCDCD">
-            <form id="frmLogin" action="ajax/logar.php" method="post" >
-                <fieldset class="form-horizontal" >
+        <div id="divforms" class="well-small offset5"  align="center" style="width: 500px; background-color:#CDCDCD">
+            <form id="frmLogin" action="ajax/logar.php" method="post">
+                <fieldset  class="form-horizontal">
                     <legend align="center" style="font: menu"><h1>Entrar</h1></legend>
 
                     <div class="loader" style="display: none;"><img src="image/loader.gif" alt="Carregando"></img></div>
@@ -15,22 +20,22 @@
 
                     <div class="control-group">
                         <label for="login" class="control-label"><b>Usuario</b></label> 
-                        <div class="controls">
-                            <input type="text" id="login" name="login" placeholder="E-mail" value="kaciano_ghelere@yahoo.com.br" style="height: 30px;"></input>
+                        <div class="controls">                            
+                            <input class="input-xlarge" type="text" id="login" name="login" placeholder="E-mail" value="kaciano_ghelere@yahoo.com.br" style="height: 30px;"></input>
                         </div>
                     </div>
 
                     <div class="control-group">
                         <label for="senha" class="control-label"><b>Senha</b></label> 
                         <div class="controls">
-                            <input type="password" id="senha" placeholder="******" name="senha" value="123456" style="height:30px;"></input>
+                            <input class="input-xlarge" type="password" id="senha" placeholder="******" name="senha" value="123456" style="height:30px;"></input>
                         </div>
                     </div>
 
-                    <div class="form-actions" style="background-color:#CDCDCD">
-                        <input class="btn btn-info" type="submit" value="Entrar"></input>
-                    </div>
                 </fieldset>
+                <div class="form-actions" style="background-color:#CDCDCD">
+                    <input class="btn btn-info" type="submit" value="Entrar"></input>
+                </div>
             </form>
         </div>        
     </body>
