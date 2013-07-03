38507d07780f47ee879090b9008470bd5bb1ec2d
win <win@192.168.254.81>
2013-07-03 09:02:28 -0300

    Prototipo do menu testado, ajustes dinamicos implementados Começar fase SUBMENU

--- js/prototipoMenu.js
+++ js/prototipoMenu.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-    var visivel1 = false;    
+$(document).ready(function(){           
     for (var i = 1; i <= 3; i++){
-    $("#menu"+i).click(function(){   
-        if (!visivel1) {            
-            $("#"+$(this).attr('id')+"sub").fadeIn();            
-            visivel1 = true;             
-            $("#box").html($(this).attr('id')+" aberto!");     
-        } else {
-            $("#"+$(this).attr('id')+"sub").fadeOut();
-            visivel1 = false;
-            $("#box").html($(this).attr('id')+" fechado!");           
-        }    
-    });} 
+        $("#menu"+i).click(function(){   
+            if ($("#"+$(this).attr('id')+"sub").css('display') === 'none') {                        
+                $("#"+$(this).attr('id')+"sub").fadeIn();                    
+                $("#"+$(this).attr('id')+"sub").css('display','inline-block');
+                $("#box").html($(this).attr('id')+" aberto!"); 
+            } else {
+                $("#"+$(this).attr('id')+"sub").fadeOut();
+                $("#"+$(this).attr('id')+"sub").css('display','none');
+                $("#box").html($(this).attr('id')+" fechado!");  
+            }               
+        });        
+    } 
 });
--- subMenus.php
+++ subMenus.php
@@ -1,5 +1,5 @@
 <?php
-    function subMenu($x,$style,$quant) {        
+    function subMenu($x,$style,$quant) {     
         for ($i = 0; $i < $quant; $i++){
             echo '<a class="btn btn-'.$style.'" id="sub'.$x.$i.'">Sub-Menu '.$x.$i.'</a>'; 
         }
