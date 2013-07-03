6af2aceafc6aa34ba298542c459f6b54d8d642e8
win <win@192.168.254.81>
2013-07-03 11:30:44 -0300

    Prototipo do menu testado, ajustes dinamicos implementados Começar fase SUBMENU

--- js/prototipoMenu.js
+++ js/prototipoMenu.js
@@ -3,13 +3,28 @@ $(document).ready(function(){
         $("#menu"+i).click(function(){   
             if ($("#"+$(this).attr('id')+"sub").css('display') === 'none') {                        
                 $("#"+$(this).attr('id')+"sub").fadeIn();                    
-                $("#"+$(this).attr('id')+"sub").css('display','inline-block');
+                $("#"+$(this).attr('id')+"sub").css('display','inline-block');                
                 $("#box").html($(this).attr('id')+" aberto!"); 
             } else {
                 $("#"+$(this).attr('id')+"sub").fadeOut();
-                $("#"+$(this).attr('id')+"sub").css('display','none');
+                $("#"+$(this).attr('id')+"sub").css('display','none');                
                 $("#box").html($(this).attr('id')+" fechado!");  
             }               
         });        
-    } 
+    }
+    for (var i = 1; i <= 3; i++){
+        for (var j = 1; j <= 6; j++){
+            $("#sub"+i+j).click(function(){                  
+                if ($("#mini"+$(this).attr('id')).css('display') === 'none') {
+                    $("#box").html($(this).attr('id')+" aberto!");      
+                    $("#mini"+$(this).attr('id')).fadeIn(); 
+                    $("#mini"+$(this).attr('id')).css('display','inline-block');
+                } else {
+                    $("#box").html($(this).attr('id')+" fechado!");      
+                    $("#mini"+$(this).attr('id')).fadeOut(); 
+                    $("#mini"+$(this).attr('id')).css('display','none'); 
+                }                 
+            });        
+        }
+    }    
 });
--- subMenus.php
+++ subMenus.php
@@ -1,20 +1,26 @@
 <?php
+    function miniMenu($x,$quant){
+        for ($i = 1; $i <= $quant; $i++){
+            echo '<a class="btn" id="mini'.$x.$i.'">MiniSub '.$x.$i.'</a>';
+        }
+    }
     function subMenu($x,$style,$quant) {     
-        for ($i = 0; $i < $quant; $i++){
+        for ($i = 1; $i <= $quant; $i++){
             echo '<a class="btn btn-'.$style.'" id="sub'.$x.$i.'">Sub-Menu '.$x.$i.'</a>'; 
+            echo '<div id="minisub'.$x.$i.'" style="display: none;">';
+                miniMenu("$x"."$i",4);
+            echo '</div>';
         }
     }
     function menu($j,$quant){
         $style = ["success","info","danger","inverse","warning","btn"];        
         for ($i = 1; $i <= $j; $i++){    
-            echo "<div>
-                    <a class='btn btn-".$style[$i-1]."' id='menu".$i."'>Menu Principal ".$i."</a>
-                    <div id='menu".$i."sub' style='display: none;'>";
-            
-            subMenu($i,$style[$i-1],$quant);                          
-            
-            echo "</div>
-            </div> ";
+            echo "<div>";
+                echo "<a class='btn btn-".$style[$i-1]."' id='menu".$i."'>Menu Principal ".$i."</a>";
+                echo "<div id='menu".$i."sub' style='display: none;'>";            
+                    subMenu($i,$style[$i-1],$quant);            
+                echo "</div>";
+            echo "</div>";
         }
     }
 ?>
